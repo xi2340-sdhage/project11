0bdf7f7eed27c8f7477895735a9d9b7f6fc56b0d
xi2340-sdhage <107611942+xi2340-sdhage@users.noreply.github.com>
2023-06-16 17:13:47 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,13 @@ echo "Hello"
 git clone https://github.com/xi2340-sdhage/project11.git
 
 cd project11
+git config --global user.email "sujit.dhage@xebia.com"
+
+git config --global user.name "sujeetdhage7171"
+
+git add .
+
+git commit -m "pushing code"
 
 git remote add bitbucket https://bitbucket.org/sujeetdhage71711/target11.git
 
