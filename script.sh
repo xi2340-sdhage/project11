28d8eedabced76f29eca9253a4a040b5b9988f98
xi2340-sdhage <107611942+xi2340-sdhage@users.noreply.github.com>
2023-06-16 19:33:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,17 +5,17 @@ git clone https://github.com/xi2340-sdhage/project11.git
 
 cd project11
 
-git config --global user.email "dhagesujit11feb@gmail.com"
+#git config --global user.email "dhagesujit11feb@gmail.com"
 
-git config --global user.name "sujeet1111"
+#git config --global user.name "sujeet1111"
 
-git add .
+#git add .
 
-git commit -m "pushing code"
+#git commit -m "pushing code"
 
 git remote remove origin
 
-git remote add origin https://sujeet1111:Ganesha\$111711@bitbucket.org/yblsujeet/target11.git
+git remote add origin https://sujeet1111:ATBBASXkpkt5XLJN7TE9C4W8tsUwCA9C2155@bitbucket.org/yblsujeet/target17.git
 #git remote add origin https://bitbucket.org/yblsujeet/target11.git
 #git remote add origin https://bitbucket.org/yblsujeet/ybl/target11.git
 #git remote add origin https://bitbucket.org/sujeetdhage7171/target11.git
