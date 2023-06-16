bd7fce5f3c4f2e3d3a4a2c423cd37cc41a74354e
xi2340-sdhage <107611942+xi2340-sdhage@users.noreply.github.com>
2023-06-16 15:50:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "Hello"
+
+
+git clone https://github.com/xi2340-sdhage/project11.git
+
+cd project11
+
+git remote add bitbucket https://bitbucket.org/sujeetdhage71711/target11.git
+
+git push -u bitbucket main
+
+ls -lrt
