630a7ba582757ecb06a3af372c8e0447c4203e69
xi2340-sdhage <107611942+xi2340-sdhage@users.noreply.github.com>
2023-06-16 17:54:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,7 +15,7 @@ git commit -m "pushing code"
 
 git remote remove origin
 
-git remote add origin bitbucket https://bitbucket.org/yblsujeet/ybl/target11.git
+git remote add origin https://bitbucket.org/yblsujeet/ybl/target11.git
 #git remote add origin https://bitbucket.org/sujeetdhage7171/target11.git
 
 git push -u origin main
