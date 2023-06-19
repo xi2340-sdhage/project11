b24561c982d613588ff2a08b9eed50217a1e3c80
xi2340-sdhage <107611942+xi2340-sdhage@users.noreply.github.com>
2023-06-19 15:12:43 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,22 +7,27 @@ git clone https://github.com/xi2340-sdhage/project11.git
 cd project11
 
 # Add the Bitbucket remote
-git remote remove origin
-
-git remote add origin https://sujeet1111:ATBBASXkpkt5XLJN7TE9C4W8tsUwCA9C2155@bitbucket.org/yblsujeet/target17.git
-
+git remote add bitbucket https://sujeet1111:ATBBASXkpkt5XLJN7TE9C4W8tsUwCA9C2155@bitbucket.org/yblsujeet/target17.git
 
 # Fetch changes from GitHub and Bitbucket
-#git pull origin main
-#git pull bitbucket --all
+git fetch origin
+git fetch bitbucket
 
-# Merge the changes from GitHub
-#git merge origin/main
-
-# Merge the changes from Bitbucket
-#git merge bitbucket/main
-
-# Push the changes to Bitbucket
-git push --all -u origin --force
+# Loop through all branches in the GitHub repository
+for branch in $(git branch -r | grep -v '\->'); do
+  # Check out each branch
+  git checkout "${branch#origin/}"
+  
+  # Merge the changes from GitHub
+  git merge origin/"${branch#origin/}"
+  
+  git push -u origin "${branch#origin/}"
+  
+  # Merge the changes from Bitbucket
+  git merge bitbucket/"${branch#origin/}"
+  
+  # Push the changes to Bitbucket
+  git push -u bitbucket "${branch#origin/}"
+done
 
 ls -lrt
