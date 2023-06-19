9200eaa9e822e73e87b30bc703b0521da3babd3c
xi2340-sdhage <107611942+xi2340-sdhage@users.noreply.github.com>
2023-06-19 11:34:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,8 @@ git clone https://github.com/xi2340-sdhage/project11.git
 
 cd project11
 
+git remote remove origin
+
 git remote add origin https://sujeet1111:ATBBASXkpkt5XLJN7TE9C4W8tsUwCA9C2155@bitbucket.org/yblsujeet/target17.git
 
 git push -u origin main
