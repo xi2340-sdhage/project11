d5f658e9a3b2d5fba9914c678b60e866d6e71275
xi2340-sdhage <107611942+xi2340-sdhage@users.noreply.github.com>
2023-06-19 14:58:08 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,10 @@ git clone https://github.com/xi2340-sdhage/project11.git
 cd project11
 
 # Add the Bitbucket remote
-git remote add bitbucket https://sujeet1111:ATBBASXkpkt5XLJN7TE9C4W8tsUwCA9C2155@bitbucket.org/yblsujeet/target17.git
+git remote remove origin
+
+git remote add origin https://sujeet1111:ATBBASXkpkt5XLJN7TE9C4W8tsUwCA9C2155@bitbucket.org/yblsujeet/target17.git
+
 
 # Fetch changes from GitHub and Bitbucket
 git pull --all
