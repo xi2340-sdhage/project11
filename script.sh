773b4408eefbfce8c9d1f67ec968ad4e611d2236
xi2340-sdhage <107611942+xi2340-sdhage@users.noreply.github.com>
2023-06-19 12:03:01 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Hello"
 
-git fetch --update-shallow https://github.com/xi2340-sdhage/project11.git
+git fetch https://github.com/xi2340-sdhage/project11.git
 
 #cd project11
 
@@ -9,8 +9,8 @@ git remote remove origin
 
 git remote add origin https://sujeet1111:ATBBASXkpkt5XLJN7TE9C4W8tsUwCA9C2155@bitbucket.org/yblsujeet/target17.git
 
-git pull origin main
+git pull origin main --allow-unrelated-histories
 
-git push -u origin main
+git push -u origin main --force
 
 ls -lrt
