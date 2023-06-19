c03517572c2471e24e28c3e5d151973637e4a571
xi2340-sdhage <107611942+xi2340-sdhage@users.noreply.github.com>
2023-06-19 12:13:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+
 echo "Hello"
 
-git clone https://github.com/xi2340-sdhage/project11.git
+git remote add github https://github.com/xi2340-sdhage/project11.git
 
-cd project11
+git remote add bitbucket https://sujeet1111:ATBBASXkpkt5XLJN7TE9C4W8tsUwCA9C2155@bitbucket.org/yblsujeet/target17.git
 
-git remote remove origin
+git fetch github
 
-git remote add origin https://sujeet1111:ATBBASXkpkt5XLJN7TE9C4W8tsUwCA9C2155@bitbucket.org/yblsujeet/target17.git
+git merge github/main
 
-git pull origin main --allow-unrelated-histories
-
-git push -u origin main --force
+git push -u bitbucket main
 
 ls -lrt
